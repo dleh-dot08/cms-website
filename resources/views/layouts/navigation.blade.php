698b71925d907c4054b99505be088fe0f2f1e1e4
Frrama <arjunafadli@gmail.com>
2026-01-06 16:35:40 +0700

    Add about page view template

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -46,7 +46,11 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-900" />
+                        <img 
+                            src="{{ asset('img/logo/ASN_logo-color.png') }}" 
+                            alt="Logo ASN"
+                            class="h-9 w-auto"
+                        >
                     </a>
                 </div>
 
